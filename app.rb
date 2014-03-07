efe3139f12d895c473af5076992d8a23f21f5cb4
klebervirgilio <klebervirgilio@gmail.com>
2014-03-07 10:42:16 -0300

    fx

--- app.rb
+++ app.rb
@@ -6,16 +6,16 @@ class Casamento < Sinatra::Base
   DAY_IN_HOURS      = 24
   HOUR_IN_SECONDS   = 3600
   MINUTE_IN_SECONDS = 60
-  
+
   get '/' do
-    @time = time = Time.local(0,0,11,21,4,2012,nil,nil,nil,-3) - Time.now
+    @time = time = Time.local(0,0,11,18,4,2014,nil,nil,nil,-3) - Time.now
     @counting = [
       @days     = Integer(time  / DAY_IN_SECONDS    ),
       @hours    = Integer((time / HOUR_IN_SECONDS   ) - (@days * DAY_IN_HOURS)),
       @minutes  = Integer((time / MINUTE_IN_SECONDS ) - (@days * DAY_IN_MINUTES) - (@hours * MINUTE_IN_SECONDS)),
       @seconds  = Integer(time  % MINUTE_IN_SECONDS ),
     ]
-    
+
     erb :app
   end
 end
